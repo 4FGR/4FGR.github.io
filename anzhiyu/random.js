36d2ec3b15ce6623443a6d2a8e152799d3614313
4FGR <sc15086814639@outlook.com>
2024-11-22 01:15:15 +0800

    Site updated: 2024-11-22 01:15:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/19/hello-world/"];function toRandomPost(){
+var posts=["2024/11/19/第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
