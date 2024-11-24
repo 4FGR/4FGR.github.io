cf91bcc223559e522abe7bfa5d4a1a5e5cd4ef1d
4FGR <sc15086814639@outlook.com>
2024-11-25 00:52:51 +0800

    Site updated: 2024-11-25 00:52:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/24/2024-11-24-各编程语言的运算符优先级/","2024/11/19/hello-world/"];function toRandomPost(){
+var posts=["2024/11/19/hello-world/","2024/11/24/2024-11-24-各编程语言的运算符优先级/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
