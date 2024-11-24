ad734844c824d342b429a8acd8b1fa0bb80893f2
4FGR <sc15086814639@outlook.com>
2024-11-24 21:29:01 +0800

    Site updated: 2024-11-24 21:29:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/19/hello-world/","2024/11/24/2024-11-24-各编程语言的符号优先级图标/"];function toRandomPost(){
+var posts=["2024/11/24/2024-11-24-各编程语言的符号优先级图标/","2024/11/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
