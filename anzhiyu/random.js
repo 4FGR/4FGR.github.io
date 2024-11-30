e5bf5c7628a86630ceec0b44c1a2e53c599468cc
4FGR <sc15086814639@outlook.com>
2024-12-01 03:10:20 +0800

    Site updated: 2024-12-01 03:10:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/24/2024-11-24-各编程语言的运算符优先级/","2024/11/24/数据结构-散列表(hash-table)/","2024/11/19/hello-world/"];function toRandomPost(){
+var posts=["2024/11/24/2024-11-24-各编程语言的运算符优先级/","2024/12/01/数据结构-散列表(hash-table)/","2024/11/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
